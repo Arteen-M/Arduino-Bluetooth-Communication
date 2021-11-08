dc687a702201ec296fa182f18a86d89e2eb8eade
Arteen M <77122362+KingCasual@users.noreply.github.com>
2021-11-07 23:29:40 -0500

    Update Master.ino

--- Master.ino
+++ Master.ino
@@ -1,9 +1,9 @@
-# include <LiquidCrystal.h>
+# include <LiquidCrystal.h>  // Imports the library for the LCD screen
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 10, d7 = 9;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);  
-int Contrast = 50;
-int temp = 0;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 10, d7 = 9;  // Sets up all the ports for the LCD screen
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);  // Sets up the Object for the LCD screen, called lcd
+const int Contrast = 50;  // Defines the contrast of the LCD screen (Since I didn't set up the Potentiometer, I define the contrast as a constant)
+int temp = 0;  // The Variable for Temperature
 
 void setup() {
   // put your setup code here, to run once:
